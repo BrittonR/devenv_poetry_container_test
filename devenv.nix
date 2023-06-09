5ec1aa16a0ef637eda55de1e2e2acd3c61ba54a9
Britton Robitzsch <b@robitzs.ch>
2023-06-09 17:27:39 +0000

    add devenv and poetry fileswq

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, config, ... }:
+
+{
+  packages = [
+    pkgs.stdenv.cc.cc.lib
+    pkgs.helix
+    pkgs.neovim
+
+  ];
+  languages.python = {
+    enable = true;
+    package = pkgs.python311;
+    # venv.enable = true;
+    poetry.enable = true;
+    poetry.activate.enable = true;
+    poetry.install.enable = true;
+  };
+  containers."package".name = "package";
+
+  containers."package".version= "devenv";
+
+  containers."package".registry = "docker://";
+
+  containers."package".startupCommand = "bash";
+}
